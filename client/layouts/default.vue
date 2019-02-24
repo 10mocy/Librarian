87a42f3049025201c7ff28bf1a9ccd51724e0abb
Tomotada Saigusa <nirot1r@g-second.net>
2019-02-24 20:23:17 +0900

    reset.css追加

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -16,10 +16,12 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
+  padding: 0;
   font-weight: normal;
   font-size: 15px;
   border: none;
   text-decoration: none;
   background-color: rgba(255, 255, 255, 0);
+  list-style: none;
 }
 </style>
