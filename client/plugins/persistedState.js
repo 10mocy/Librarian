7a5d2b113643e9e9e7e2232a85e568d9f1421c65
Tomotada Saigusa <nirot1r@g-second.net>
2019-02-24 22:59:19 +0900

    vuex-persisetdstate導入

---
+++ client/plugins/persistedState.js
@@ -0,0 +1,10 @@
+import createPersistedState from 'vuex-persistedstate'
+
+export default ({ store, isHMR }) => {
+  if (isHMR) return
+  if (process.client) {
+    window.onNuxtReady(nuxt => {
+      createPersistedState()(store)
+    })
+  }
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,10 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: ['~/plugins/vue-gravatar'],
+  plugins: [
+    '~/plugins/vue-gravatar',
+    { src: '~/plugins/persistedState', ssr: false }
+  ],
 
   /*
   ** Nuxt.js modules
