8fe2e97f84de1f500658e924ffb87b6c8555bff5
Tomotada Saigusa <nirot1r@g-second.net>
2019-02-24 17:44:38 +0900

    環境移行

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,39 @@
 <template>
   <section class="container">
-    index.vue
+    <h1>Librarian <small>w/ neiro.work</small></h1>
+    <p>
+      ログインしてください
+    </p>
+    <form @submit.prevent="authorize()">
+      <span>
+        <label>ログインID</label>
+        <input type="text" v-model="login.loginId">
+      </span>
+      <span>
+        <label>パスワード</label>
+        <input type="password" v-model="login.password">
+      </span>
+      <button type="submit">ログイン</button>
+    </form>
   </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      login: {
+        loginId: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    authorize() {
+      // console.log(this.login)
+      console.log(this.login.loginId, this.login.password)
+    }
+  }
 }
 </script>
 
