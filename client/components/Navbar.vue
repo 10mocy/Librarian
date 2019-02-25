4cdb5ed375d94faaac62138e0e170e441eae13ea
Tomotada Saigusa <nirot1r@g-second.net>
2019-02-25 20:59:12 +0900

    #6 mounted時にストアを持ってくるようにした

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -6,9 +6,9 @@
         nuxt-link(to='/').nav_menu_item_link
           font-awesome-icon(icon='home').nav_menu_item_icon
       span(v-if='isLogin()')
-        li.nav_menu_item
-          v-gravatar(:hash='session.data.gravatarId', size='18').nav_menu_item_icon
-          span.nav_menu_item_label &nbsp;音色ねいろ
+        li.nav_menu_item(v-if='session')
+          v-gravatar(:hash='session.data.gravatarId', :size='18').nav_menu_item_icon
+          span.nav_menu_item_label &nbsp;{{ session.data.displayName }}
         li.nav_menu_item
           nuxt-link(to='/books/register').nav_menu_item_link
             font-awesome-icon(icon='book').nav_menu_item_icon
@@ -35,6 +35,9 @@ export default {
       session: this.$store.state.session
     }
   },
+  mounted() {
+    this.session = this.$store.state.session
+  },
   computed: {},
   methods: {
     isLogin() {
@@ -53,7 +56,8 @@ export default {
 .nav {
   margin-bottom: 30px;
   padding: 10px;
-  background-color: #222222;
+  background-color: #eeeeee;
+  border-bottom: 1px solid #e3e3e3;
 
   &_brand,
   &_menu {
@@ -62,30 +66,31 @@ export default {
   }
   &_brand {
     padding: 0 10px;
-    color: #ffffff;
+    color: #222222;
     font-size: 1.5em;
     font-weight: bold;
+    user-select: none;
   }
 
   &_menu {
     &_item {
       display: inline-block;
       padding-left: 20px;
-      color: #bbbbbb;
+      color: #585858;
 
       &_icon,
       &_label {
         vertical-align: middle;
-      }
-      &_link,
-      &_link:link,
-      &_link:visited,
-      &_link:hover {
-        color: #999999;
-        cursor: pointer;
         user-select: none;
+      }
+      &_link {
+        color: #585858;
+        cursor: pointer;
         &.nuxt-link-exact-active {
-          color: #cccccc;
+          color: #b6b6b6;
+        }
+        &:hover {
+          color: #ff8800;
         }
       }
     }
