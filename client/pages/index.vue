3704fa8bd81daf8aca45d4936c8c908321d2554d
Tomotada Saigusa <nirot1r@g-second.net>
2019-02-25 23:31:49 +0900

    index作成

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,12 +1,22 @@
 <template lang="pug">
   section.container(v-if='initialaized')
-    ul
-      li 蔵書を探す
-      li 蔵書を管理する
+    .welcome
+      small Welcome to 
+      b Librarian!
+      p {{ userData.displayName }}としてログイン中です。
+    ul.panelButton
+      .panelButton_wrap
+        .panelButton_wrap_item.panelButton_wrap_item-success
+          font-awesome-icon(icon='search').panelButton_wrap_item_icon
+          .panelButton_wrap_item_label 蔵書を探す
+      .panelButton_wrap
+        .panelButton_wrap_item.panelButton_wrap_item
+          font-awesome-icon(icon='tools').panelButton_wrap_item_icon
+          .panelButton_wrap_item_label 蔵書を管理する
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 const axios = require('axios')
 const crypto = require('crypto')
@@ -21,6 +31,11 @@ export default {
       initialaized: false
     }
   },
+  computed: {
+    ...mapGetters({
+      userData: 'session/data'
+    })
+  },
   methods: {
     ...mapActions({
       requireAuth: 'session/requireAuth'
@@ -29,5 +44,67 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.welcome {
+  margin: 10px;
+  padding: 40px 20px;
+  font-size: 3em;
+  text-align: center;
+  background-color: #efefef;
+  border-radius: 10px;
+}
+
+.panelButton {
+  font-size: 0;
+
+  &_wrap {
+    display: inline-block;
+    vertical-align: top;
+    width: 50%;
+    font-size: 1rem;
+    color: #222222;
+
+    &_item {
+      display: block;
+      margin: 10px;
+      padding: 20px;
+      background-color: #efefef;
+      border-bottom: 2px solid #c9c9c9;
+      border-radius: 10px;
+      user-select: none;
+      cursor: pointer;
+
+      &_icon,
+      &_label {
+        display: inline-block;
+        vertical-align: middle;
+      }
+
+      &_icon {
+        font-size: 2em;
+      }
+
+      &_label {
+        padding-left: 10px;
+        font-size: 1.25em;
+        font-weight: bold;
+      }
+
+      &:active {
+        background-color: #c9c9c9;
+        border-bottom: 2px solid #efefef;
+      }
+
+      &-success {
+        color: #ffffff;
+        background-color: #008ffe;
+        border-bottom: 2px solid #037cd8;
+        &:active {
+          background-color: #037cd8;
+          border-bottom: 2px solid #008ffe;
+        }
+      }
+    }
+  }
+}
 </style>
