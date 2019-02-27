046482a4fa190b266eda8aadc82ebc02b1b1bed2
Tomotada Saigusa <nirot1r@g-second.net>
2019-02-27 23:30:33 +0900

    検索仮実装

--- client/pages/books/search.vue
+++ client/pages/books/search.vue
@@ -10,15 +10,34 @@
     .row
       .row_sidebar
         .card
-          | クエリ
+          form(@submit.prevent='doSearch()').form
+            span.form_part
+              label.form_part_label 検索クエリ
+              input.form_part_input(type='text', v-model='search.query')
+            span.form_part
+              button.button.button-submit(type='submit') 検索
       .row_main
         .card
-          | 検索結果
+          .alert.alert-danger(v-if='!search.books')
+            | 蔵書が見つかりませんでした
+          table.table(v-if='search.books')
+            thead
+              tr.table_row
+                th.table_row_header #
+                th.table_row_header 書籍名
+                th.table_row_header 登録日時
+                th.table_row_header 同人?
+            tbody.table_body
+              tr.table_row(v-for='(book, index) in search.books')
+                td.table_row_data {{ String(book.hash).slice(0, 6) }}...
+                td.table_row_data {{ book.title + (book.volume !== '-1' ? ` ${book.volume}` : '') }}
+                td.table_row_data {{ book.timestamp }}
+                td.table_row_data {{ book.isDoujin === 1 ? 'はい' : 'いいえ'}}
 
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 import axios from 'axios'
 
@@ -26,15 +45,41 @@ export default {
   async beforeMount() {
     this.initialaized = await this.requireAuth()
   },
+  computed: {
+    ...mapGetters({
+      token: 'session/token',
+      userData: 'session/data'
+    })
+  },
   data() {
     return {
-      initialaized: false
+      initialaized: false,
+      search: {
+        query: '',
+        books: null
+      }
     }
   },
   methods: {
     ...mapActions({
       requireAuth: 'session/requireAuth'
-    })
+    }),
+    doSearch() {
+      axios
+        .post(
+          '/api/books/search',
+          { query: this.search.query },
+          {
+            headers: {
+              'X-Access-Token': this.token
+            }
+          }
+        )
+        .then(res => {
+          this.search.books =
+            res.data.books.length !== 0 ? res.data.books : null
+        })
+    }
   }
 }
 </script>
@@ -47,7 +92,8 @@ export default {
   &_main {
     display: inline-block;
     vertical-align: top;
-    padding: 10px;
+    padding: 0 10px;
+    padding-bottom: 10px;
     font-size: 1rem;
   }
 
@@ -57,6 +103,41 @@ export default {
 
   &_main {
     width: 75%;
+  }
+
+  @media screen and (max-width: 1280px) {
+    &_sidebar,
+    &_main {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+
+.table {
+  width: 100%;
+  border-collapse: collapse;
+
+  &_body {
+    & .table_row {
+      background-color: #ffffff;
+      &:nth-child(2n) {
+        background-color: #e0e0e0;
+      }
+
+      &_data {
+        padding: 5px;
+      }
+    }
+  }
+
+  &_row {
+    text-align: center;
+
+    &_header {
+      font-weight: bold;
+      border-bottom: 2px solid #ababab;
+    }
   }
 }
 </style>
