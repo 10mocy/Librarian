75d4ee1d32eff0410f81f475cc2133f169117208
Tomotada Saigusa <nirot1r@g-second.net>
2019-03-09 22:33:48 +0900

    検索部分デザイン作成

--- client/pages/books/search.vue
+++ client/pages/books/search.vue
@@ -5,7 +5,27 @@
         nuxt-link(to='/').breadcrumbs_item_link ホーム
       li.breadcrumbs_item 蔵書を探す
 
-    | books/search
+    .row
+      .row_side
+        .panel
+          .panel_container
+            form.form
+              span.form_part
+                label.form_part_label 検索クエリ
+                input.form_part_input(type='search')
+              label.form_part.form_part
+                input.form_part_input(type='checkbox', id='includeTitle')
+                label.form_part_label-checkbox(for='includeTitle') タイトルに含む
+              label.form_part.form_part
+                input.form_part_input(type='checkbox', id='includeRemarks')
+                label.form_part_label-checkbox(for='includeRemarks') 備考に含む
+              label.form_part.form_part
+                input.form_part_input(type='checkbox', id='includeDoujin')
+                label.form_part_label-checkbox(for='includeDoujin') 同人誌を含む
+              span.form_part
+                button(type='submit').button.button-primary 検索
+      .row_main
+        | row_main
 </template>
 
 <script>
@@ -36,3 +56,25 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.row {
+  font-size: 0;
+
+  &_side,
+  &_main {
+    padding: 0 10px;
+    display: inline-block;
+    vertical-align: top;
+    font-size: 1rem;
+  }
+
+  &_side {
+    width: 30%;
+  }
+
+  &_main {
+    width: 70%;
+  }
+}
+</style>
