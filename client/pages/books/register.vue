cee96180d6e3eed5bbda9e7b201daa726d6a184f
Tomotada Saigusa <nirot1r@g-second.net>
2019-03-10 14:20:41 +0900

    パンくずリスト追加

--- client/pages/books/register.vue
+++ client/pages/books/register.vue
@@ -1,34 +1,41 @@
 <template lang="pug">
-  section.pageContainer
-    .panel
-      h2.panel_header 書籍登録
-      .panel_container
-        .alert.alert-danger(v-if='error')
-          .alert_content {{ error.message }}
-        .alert.alert-success(v-if='data')
-          .alert_content 「{{ data.title }}{{ data.volume != -1 ? ` ${data.volume}` : '' }}」の登録が完了しました
+  section
+    ul.breadcrumbs
+      li.breadcrumbs_item
+        nuxt-link(to='/').breadcrumbs_item_link ホーム
+      li.breadcrumbs_item 蔵書の管理
+      li.breadcrumbs_item 書籍登録
+    
+    .pageContainer
+      .panel
+        h2.panel_header 書籍登録
+        .panel_container
+          .alert.alert-danger(v-if='error')
+            .alert_content {{ error.message }}
+          .alert.alert-success(v-if='data')
+            .alert_content 「{{ data.title }}{{ data.volume != -1 ? ` ${data.volume}` : '' }}」の登録が完了しました
 
-        form(@submit.prevent='register()').form
-          span.form_part
-            label.form_part_label 書籍名
-            input.form_part_input(type='text', v-model='query.title')
-          span.form_part
-            label.form_part_label 巻数
-            label.form_part
-              input.form_part_input(type='checkbox', id='isSingle', v-model='query.isSingle')
-              label.form_part_label-checkbox(for='isSingle') 単発
-            input.form_part_input(type='number', v-model='query.volume')
-          span.form_part
-            label.form_part_label ISBN
-            input.form_part_input(type='text', v-model='query.isbn')
-          span.form_part
-            label.form_part_label 備考
-            label.form_part
-              input.form_part_input(type='checkbox', id='isDoujin', v-model='query.isDoujin')
-              label.form_part_label-checkbox(for='isDoujin') 同人
-            input.form_part_input(type='text', v-model='query.remarks')
-          span.form_part
-            button(type='submit').button.button-primary 登録
+          form(@submit.prevent='register()').form
+            span.form_part
+              label.form_part_label 書籍名
+              input.form_part_input(type='text', v-model='query.title')
+            span.form_part
+              label.form_part_label 巻数
+              label.form_part
+                input.form_part_input(type='checkbox', id='isSingle', v-model='query.isSingle')
+                label.form_part_label-checkbox(for='isSingle') 単発
+              input.form_part_input(type='number', v-model='query.volume')
+            span.form_part
+              label.form_part_label ISBN
+              input.form_part_input(type='text', v-model='query.isbn')
+            span.form_part
+              label.form_part_label 備考
+              label.form_part
+                input.form_part_input(type='checkbox', id='isDoujin', v-model='query.isDoujin')
+                label.form_part_label-checkbox(for='isDoujin') 同人
+              input.form_part_input(type='text', v-model='query.remarks')
+            span.form_part
+              button(type='submit').button.button-primary 登録
 </template>
 
 <script>
