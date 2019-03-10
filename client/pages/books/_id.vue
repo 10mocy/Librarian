4e19307e279b7d5bb8d286ee524bb39558d76626
Tomotada Saigusa <nirot1r@g-second.net>
2019-03-10 22:49:15 +0900

    エラー時にもパンくずリストを表示するようにした

--- client/pages/books/_id.vue
+++ client/pages/books/_id.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
   section(v-if='initialaized')
+
+    ul.breadcrumbs
+      li.breadcrumbs_item
+        nuxt-link(to='/').breadcrumbs_item_link ホーム
+      li.breadcrumbs_item 書籍情報
+      li.breadcrumbs_item(v-if='error') {{ error.message }}
+      li.breadcrumbs_item(v-if='book') {{ book.title }}{{ book.volume != -1 ? ` ${book.volume}` : '' }}
     .alert.alert-danger(v-if='error') {{ error.message }}
 
     div(v-if='book')
-      ul.breadcrumbs
-        li.breadcrumbs_item
-          nuxt-link(to='/').breadcrumbs_item_link ホーム
-        li.breadcrumbs_item 書籍情報
-        li.breadcrumbs_item {{ book.title }}{{ book.volume != -1 ? ` ${book.volume}` : '' }}
-        
       h1.page-header
         .label(:class="{ 'label-danger': book.isDoujin == 0, 'label-primary': book.isDoujin == 1 }") {{ book.isDoujin == 0 ? '商業' : '同人' }}
         | {{ book.title }}{{ book.volume != -1 ? ` ${book.volume}` : '' }}
@@ -36,7 +37,16 @@ export default {
       })
       .then(res => {
         this.book = res.data.book
-        console.log(this.book)
+      })
+      .catch(err => {
+        this.error = {
+          message:
+            err.response.status === 403
+              ? '認証に失敗しました'
+              : err.response.status === 404
+                ? '書籍が見つかりませんでした'
+                : err.response.status
+        }
       })
   },
   computed: {
