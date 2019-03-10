dd9b0ed77c75d3d4a0f641ebc02d0a339a610423
Tomotada Saigusa <nirot1r@g-second.net>
2019-03-10 23:01:40 +0900

    削除実装

--- client/pages/books/_id.vue
+++ client/pages/books/_id.vue
@@ -5,14 +5,23 @@
       li.breadcrumbs_item
         nuxt-link(to='/').breadcrumbs_item_link ホーム
       li.breadcrumbs_item 書籍情報
-      li.breadcrumbs_item(v-if='error') {{ error.message }}
       li.breadcrumbs_item(v-if='book') {{ book.title }}{{ book.volume != -1 ? ` ${book.volume}` : '' }}
+
     .alert.alert-danger(v-if='error') {{ error.message }}
+    .alert.alert-success(v-if='status') {{ status.message }}
 
     div(v-if='book')
       h1.page-header
         .label(:class="{ 'label-danger': book.isDoujin == 0, 'label-primary': book.isDoujin == 1 }") {{ book.isDoujin == 0 ? '商業' : '同人' }}
         | {{ book.title }}{{ book.volume != -1 ? ` ${book.volume}` : '' }}
+
+      .row
+        .row_main
+          | row_main
+        .row_side
+          button.button.button-lg.button-danger(@click='remove()')
+            font-awesome-icon(icon='trash')
+            | &nbsp;書籍を削除する
 </template>
 
 <script>
@@ -24,6 +33,7 @@ export default {
     return {
       initialaized: false,
       error: null,
+      status: null,
       book: null
     }
   },
@@ -58,10 +68,62 @@ export default {
   methods: {
     ...mapActions({
       requireAuth: 'session/requireAuth'
-    })
+    }),
+    remove() {
+      this.status = null
+      this.error = null
+
+      axios
+        .delete(`/api/books/${this.$route.params.id}`, {
+          headers: {
+            'X-Access-Token': this.token
+          }
+        })
+        .then(res => {
+          console.log(res)
+          this.status = {
+            message: '削除に成功しました'
+          }
+        })
+        .catch(err => {
+          this.error = {
+            message:
+              err.response.status === 403
+                ? '認証に失敗しました'
+                : err.response.status === 404
+                  ? '書籍が見つかりませんでした'
+                  : err.response.status
+          }
+        })
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.row {
+  font-size: 0;
+
+  &_side,
+  &_main {
+    display: inline-block;
+
+    vertical-align: top;
+    font-size: 1rem;
+
+    @media screen and (max-width: 600px) {
+      display: block;
+      width: 100% !important;
+    }
+  }
+
+  &_main {
+    width: 70%;
+    margin-bottom: 10px;
+  }
+
+  &_side {
+    width: 30%;
+  }
+}
 </style>
